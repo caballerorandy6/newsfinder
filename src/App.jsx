602f0cee688d58b2be3db43cf3d999f16b1be700
Randy Caballero <caballerorandy6@gmail.com>
2022-12-01 00:29:45 -0600

    hasta la clase 324

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Container, Grid, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
+import { NoticiasProvider } from "./context/NoticiasProvider";
+import ListadoNoticias from "./components/ListadoNoticias";
+
+function App() {
+  return (
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h1">
+            News Finder
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6}>
+            <Formulario />
+          </Grid>
+        </Grid>
+
+        <ListadoNoticias />
+      </Container>
+    </NoticiasProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,37 @@
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import useNoticias from "../hooks/useNoticias";
+
+const CATEGORIAS = [
+  { value: "general", label: "General" },
+  { value: "business", label: "Business" },
+  { value: "entertainment", label: "Entertainment" },
+  { value: "health", label: "Health" },
+  { value: "science", label: "Science" },
+  { value: "sports", label: "Sports" },
+  { value: "technology", label: "Technology" },
+];
+
+const Formulario = () => {
+  const { categoria, handleChangeCategoria } = useNoticias();
+
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Category</InputLabel>
+        <Select
+          label="Category"
+          onChange={handleChangeCategoria}
+          value={categoria}
+        >
+          {CATEGORIAS.map((categoria) => (
+            <MenuItem key={categoria.value} value={categoria.value}>
+              {categoria.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </form>
+  );
+};
+
+export default Formulario;
